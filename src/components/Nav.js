f9a7a20458c90e2e58c3d2130ed44b8e4c56273d
Allan <martinezallan123@gmail.com>
2023-01-20 15:48:41 -0800

    rename + refactor

---
+++ src/components/Nav.js
@@ -0,0 +1,47 @@
+//components
+import OpenBoardCreationBtn from './OpenBoardCreationBtn'
+import LightDarkToggle from './LightDarkToggle';
+//images
+import logo from '../assets/logo-mobile.svg'
+import chevronDown from '../assets/icon-chevron-down.svg'
+import verticalEllipsis from '../assets/icon-vertical-ellipsis.svg'
+//functions
+import { capitalizeFirstLetter } from '../App';
+
+function Nav({ focusedBoard, openDesc, openCreateBoard, openBoardList }) {
+  return (
+    <nav>
+      <div className="flex-between title-container">
+        <img src={logo} alt="Logo" />
+        { focusedBoard ? 
+          <h1>
+            { focusedBoard.title ? focusedBoard.title
+              .split(" ")
+              .map(capitalizeFirstLetter)
+              .join(" ")
+              : "Platform Launch"
+            } 
+          </h1>
+          : <h1>Platform Launch</h1>
+       }
+      <div className="flex-between controls-container ">
+        <img 
+          src={chevronDown} 
+          className="chevron-down"
+          alt="Switch boards button"
+          onClick={ () => openBoardList()} />
+        <img 
+          className="vertical-ellipsis" 
+          src={verticalEllipsis} 
+          alt="Edit current task" 
+          onClick={ () => openDesc() } />
+        <OpenBoardCreationBtn 
+          openCreateBoard={openCreateBoard} />
+        </div>
+      </div>
+        <LightDarkToggle />
+    </nav>
+  );
+}
+
+export default Nav;
